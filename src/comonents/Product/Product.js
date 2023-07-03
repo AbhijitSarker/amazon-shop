a74ce66e0cd8fa79b24a6a08c4b7d68bef42986f
Abhijitsarker <abhijitsarker03@gmail.com>
2023-07-04 01:11:10 +0600

    products section

---
+++ src/comonents/Product/Product.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+    const { name, img, seller, price, ratings } = props.product;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className="product-info">
+                <p className='product-name'>{name}</p>
+
+                <p>Price: ${price}</p>
+                <p>Ratings: {ratings} Stars</p>
+                <p> <small>Seller: {seller}</small> </p>
+            </div>
+
+        </div>
+    );
+};
+
+export default Product;
