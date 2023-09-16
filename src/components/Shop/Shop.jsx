bcb318b4fca3a402b8cbbf466b690ffc334b7c75
AbhijitSarker <abhijitsarker03@gmail.com>
2023-09-17 01:43:20 +0600

    pagination btns

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,11 +3,23 @@ import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fa
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([])
+    const [cart, setCart] = useState([]);
+
+    const { totalProducts } = useLoaderData();
+
+
+    //pagination-----------------------
+    const itemsPerPage = 10;
+    const totalPages = Math.ceil(totalProducts / itemsPerPage);
+
+    const pageNumbers = [...Array(totalPages).keys()];
+
+
+
 
     useEffect(() => {
         fetch('http://localhost:3000/products')
@@ -62,27 +74,36 @@ const Shop = () => {
     }
 
     return (
-        <div className='shop-container'>
-            <div className="products-container">
+        <>
+            <div className='shop-container'>
+                <div className="products-container">
+                    {
+                        products.map(product => <Product
+                            key={product._id}
+                            product={product}
+                            handleAddToCart={handleAddToCart}
+                        ></Product>)
+                    }
+                </div>
+                <div className="cart-container">
+                    <Cart
+                        cart={cart}
+                        handleClearCart={handleClearCart}
+                    >
+                        <Link className='proceed-link' to="/orders">
+                            <button className='btn-proceed'>Review Order</button>
+                        </Link>
+                    </Cart>
+                </div>
+            </div>
+
+            {/* pagination */}
+            <div className="pagination">
                 {
-                    products.map(product => <Product
-                        key={product._id}
-                        product={product}
-                        handleAddToCart={handleAddToCart}
-                    ></Product>)
+                    pageNumbers.map(number => <button key={number}>{number}</button>)
                 }
             </div>
-            <div className="cart-container">
-                <Cart
-                    cart={cart}
-                    handleClearCart={handleClearCart}
-                >
-                    <Link className='proceed-link' to="/orders">
-                        <button className='btn-proceed'>Review Order</button>
-                    </Link>
-                </Cart>
-            </div>
-        </div>
+        </>
     );
 };
 
